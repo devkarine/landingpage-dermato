14ef054386926829e182cca2955431b45e84f4dd
Karine Pereira <karinedeveloper@gmail.com>
2023-05-26 16:45:35 -0300

    feat: implementando corrousel na sections de comments

--- src/js/script.js
+++ src/js/script.js
@@ -28,3 +28,28 @@ question.forEach((item) => {
     item.classList.toggle("ativo");
   });
 });
+
+
+var carousel = document.querySelector(".carousel");
+      var cardWidth = document.querySelector(".card").offsetWidth;
+      var offset = 0;
+
+      function moveLeft() {
+        offset += cardWidth + 20;
+        if (offset > 0) {
+          offset = 0;
+        }
+        carousel.style.transform = "translateX(" + offset + "px)";
+      }
+
+      function moveRight() {
+        offset -= cardWidth + 20;
+        var maxOffset = -(carousel.scrollWidth - carousel.offsetWidth - 20);
+        if (offset < maxOffset) {
+          offset = maxOffset;
+        }
+        carousel.style.transform = "translateX(" + offset + "px)";
+      }
+
+      document.querySelector(".nav-left").addEventListener("click", moveLeft);
+      document.querySelector(".nav-right").addEventListener("click", moveRight);
